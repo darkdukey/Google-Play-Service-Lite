f3aa6e2d4c96304727c37bc416efa873bfd0c72a
yinjimmy <jimmy.yin5@gmail.com>
2016-06-07 14:09:59 +0800

    update gms

--- pack_gms.sh
+++ pack_gms.sh
@@ -17,65 +17,82 @@ PLAY_SERVICES_STRIP_FILE="config.conf"
 PLAY_SERVICES_NESTED_PATH="com/google/android/gms"
 PLAY_SERVICES_OUTPUT_FILE="google-play-services-full.jar"
 
-# Check if file exists in the same directory
-if [ ! -d $PLAY_SERVICES_PATH ]; then
-    echo "\nPlease config $ANDROID_SDK_ROOT, then run it again\n\n"
-    exit -1
-fi
+pack_gms() {
+    echo ""
+    echo "Packing google play services ..."
+    echo ""
 
-# Preventive cleanup
-rm -rf $PLAY_SERVICES_TEMP_DIR
+    PLAY_SERVICES_STRIP_FILE=$1
+    PLAY_SERVICES_OUTPUT_FILE=$2
 
-# Create temporary work folder
-mkdir $PLAY_SERVICES_TEMP_DIR
-cd $PLAY_SERVICES_TEMP_DIR
-
-# If the configuration file doesn't exist, create it
-if [ ! -f ../$PLAY_SERVICES_STRIP_FILE ]; then
-    # Create the file
-    touch ../$PLAY_SERVICES_STRIP_FILE
-    FOLDERS=`ls $PLAY_SERVICES_PATH`
-    for index in $FOLDERS
-    do
-        echo "$index=true" >> ../$PLAY_SERVICES_STRIP_FILE
-    done
-
-    sed "1d" ../$PLAY_SERVICES_STRIP_FILE > ../$PLAY_SERVICES_STRIP_FILE.bak
-    sed "s/play-services-//g" ../$PLAY_SERVICES_STRIP_FILE.bak > ../$PLAY_SERVICES_STRIP_FILE
-fi
-
-# Read configuration from file
-while read -r FOLDERS
-do
-    CURRENT_FOLDER=$FOLDERS
-    CURRENT_FOLDER_NAME=`echo $CURRENT_FOLDER | awk -F'=' '{print $1}'`
-    CURRENT_FOLDER_ENABLED=`echo $CURRENT_FOLDER | awk -F'=' '{print $2}'`
-
-    if [ "$CURRENT_FOLDER_ENABLED" = true ]; then
-        ARR_FILE=$PLAY_SERVICES_PATH/play-services-$CURRENT_FOLDER_NAME/$PLAY_SERVICES_VERSION/play-services-$CURRENT_FOLDER_NAME-$PLAY_SERVICES_VERSION.aar
-
-        echo "Extracting " $CURRENT_FOLDER_NAME
-        if [ -f $ARR_FILE ]; then
-            unzip $ARR_FILE -d $CURRENT_FOLDER_NAME      > /dev/null
-        fi
-        if [ -f $CURRENT_FOLDER_NAME/classes.jar ]; then
-            jar xvf $CURRENT_FOLDER_NAME/classes.jar     > /dev/null
-        fi
-        echo "Extracting Done\n"
-    else
-        echo "Skip extracting " $CURRENT_FOLDER_NAME "\n"
+    # Check if file exists in the same directory
+    if [ ! -d $PLAY_SERVICES_PATH ]; then
+        echo "\nPlease config $ANDROID_SDK_ROOT, then run it again\n\n"
+        exit -1
     fi
-done < ../"$PLAY_SERVICES_STRIP_FILE"
 
-# Create final stripped JAR
-jar cf $PLAY_SERVICES_OUTPUT_FILE com/
-cp $PLAY_SERVICES_OUTPUT_FILE ../$PLAY_SERVICES_FILENAME
-cd ..
+    # Preventive cleanup
+    rm -rf $PLAY_SERVICES_TEMP_DIR
 
-# Clean up
-echo "\nFolders removed, cleaning up.."
-rm -rf $PLAY_SERVICES_TEMP_DIR
-rm -fr $PLAY_SERVICES_STRIP_FILE.bak
+    # Create temporary work folder
+    mkdir $PLAY_SERVICES_TEMP_DIR
+    cd $PLAY_SERVICES_TEMP_DIR
 
-echo "All done, exiting!"
-exit 0
+    # If the configuration file doesn't exist, create it
+    if [ ! -f ../$PLAY_SERVICES_STRIP_FILE ]; then
+        # Create the file
+        touch ../$PLAY_SERVICES_STRIP_FILE
+        FOLDERS=`ls $PLAY_SERVICES_PATH`
+        for index in $FOLDERS
+        do
+            echo "$index=true" >> ../$PLAY_SERVICES_STRIP_FILE
+        done
+
+        sed "1d" ../$PLAY_SERVICES_STRIP_FILE > ../$PLAY_SERVICES_STRIP_FILE.bak
+        sed "s/play-services-//g" ../$PLAY_SERVICES_STRIP_FILE.bak > ../$PLAY_SERVICES_STRIP_FILE
+    fi
+
+    # Read configuration from file
+    while read -r FOLDERS
+    do
+        CURRENT_FOLDER=$FOLDERS
+        CURRENT_FOLDER_NAME=`echo $CURRENT_FOLDER | awk -F'=' '{print $1}'`
+        CURRENT_FOLDER_ENABLED=`echo $CURRENT_FOLDER | awk -F'=' '{print $2}'`
+
+        if [ "$CURRENT_FOLDER_ENABLED" = true ]; then
+            ARR_FILE=$PLAY_SERVICES_PATH/play-services-$CURRENT_FOLDER_NAME/$PLAY_SERVICES_VERSION/play-services-$CURRENT_FOLDER_NAME-$PLAY_SERVICES_VERSION.aar
+
+            echo "Extracting " $CURRENT_FOLDER_NAME
+            if [ -f $ARR_FILE ]; then
+                unzip $ARR_FILE -d $CURRENT_FOLDER_NAME      > /dev/null
+            fi
+            if [ -f $CURRENT_FOLDER_NAME/classes.jar ]; then
+                jar xvf $CURRENT_FOLDER_NAME/classes.jar     > /dev/null
+            fi
+            echo "Extracting Done."
+        else
+            echo "Skip extracting " $CURRENT_FOLDER_NAME
+        fi
+    done < ../"$PLAY_SERVICES_STRIP_FILE"
+
+    # Create final stripped JAR
+    jar cf $PLAY_SERVICES_OUTPUT_FILE com/
+    cp $PLAY_SERVICES_OUTPUT_FILE ../
+    cd ..
+
+    # Clean up
+    echo "\nFolders removed, cleaning up.."
+    rm -rf $PLAY_SERVICES_TEMP_DIR
+    rm -fr $PLAY_SERVICES_STRIP_FILE.bak
+}
+
+main() {
+    pack_gms "gms_full.conf" "google-play-services-full.jar"
+    rm -fr "gms_full.conf"
+    pack_gms "gms_lite.conf" "google-play-services.jar"
+
+    echo "All done, exiting!"
+    exit 0
+}
+
+main
